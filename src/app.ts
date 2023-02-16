bebb5bf491b2b581b7cb6dc21a2a2f4419c79722
Brandt Davis <brandtd77@gmail.com>
2023-02-15 22:12:13 -0700

    Implement abstract class

--- src/app.ts
+++ src/app.ts
@@ -79,7 +79,6 @@ function validate(validatableInput: Validatable) {
         isValid = 
             isValid && validatableInput.value.length > validatableInput.minLength;
             console.log(validatableInput.value.length)
-            
     }
 
     if(
@@ -122,6 +121,47 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     }
     return adjustedDescriptor
 }
+
+// Component Base Class
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+        templateId: string, 
+        hostElementId: string, 
+        insertAtStart: boolean,
+        newElementId?: string
+    ) { 
+        this.templateElement = document.getElementById(
+            templateId
+        )! as HTMLTemplateElement;
+        this.hostElement = document.getElementById(hostElementId)! as T;
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNode.firstElementChild as U;
+        if(newElementId){
+            this.element.id = newElementId;
+        }
+
+        this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBeginning: boolean) {
+        this.hostElement.insertAdjacentElement(
+            insertAtBeginning ? 'afterbegin' : 'beforeend', 
+            this.element
+        )
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+}
+
 
 // ProjectList class
 class ProjectList {
