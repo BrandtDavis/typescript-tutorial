e5f4265b9282f969d5b7692ca91a7b36a8ceb149
Brandt Davis <brandtd77@gmail.com>
2023-02-23 20:36:51 -0700

    Add droppable area to UI

--- src/app.ts
+++ src/app.ts
@@ -201,7 +201,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
 
     @autobind
     dragStartHandler(event: DragEvent) {
-        console.log(event);
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent){
@@ -230,8 +231,6 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         this.assignedProjects = [];
         this.element.id = `${this.type}-projects`;
 
-
-
         this.configure();
         this.renderContent();
     }
@@ -245,14 +244,21 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     }
 
     @autobind
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
+    }
+
+    dropHandler(event: DragEvent) {
+        console.log(event.dataTransfer!.getData('text/plain'))
     }
 
     dragLeaveHandler(_: DragEvent){
         const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+        listEl.classList.remove('droppable');
     }
 
     configure() {
